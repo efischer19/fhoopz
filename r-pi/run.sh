14eb7d2b43a503a5e7eb5ab27c151183e1dfb6eb
Eric Fischer <efischer0119@gmail.com>
2020-02-16 18:50:52 -0500

    now a functional crontab script

--- r-pi/run.sh
+++ r-pi/run.sh
@@ -1,4 +1,4 @@
-source /home/pi/fhoopz/venv/bin/activate
+. /home/pi/fhoopz/venv/bin/activate
 date >> /home/pi/fhoopz/daily_run.log
-python3 /home/pi/fhoopz/daily_truth_update.py >> /home/pi/fhoopz/daily_run.log
+python3 /home/pi/fhoopz/daily_truth_update.py >> /home/pi/fhoopz/daily_run.log 2>&1
 deactivate
